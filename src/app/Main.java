513479b744cb03d3ecec5430c6fe69d92d61a50e
Marco Túlio Duarte <109878566+duartemarco@users.noreply.github.com>
2024-04-29 21:07:30 -0300

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,43 +5,18 @@ import java.awt.image.BufferedImage;
 public class Main {
     public static void main(String[] args) {
         BufferedImage imagem = ManipulaImagem.abrirImagem("menino.png");
+        double[] kernelGaussiano = {0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625};
+        int[] linhasHorizontais = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
+        int[] linhasVerticais = {-1, 0, 1, -2, 0, 0, 0, 1, 2, 1};
+        int[] linhasOeste = {1, 1, -1, 1, -2, -1, 1, 1, -1};
+        int[] laplaciano = {0, -1, 0, -1, 4, };
 
-        ManipulaImagem.exibirImagens(
-                imagem,
-                //OpPontuais.bandaRed(imagem),
-                //OpPontuais.bandaGreen(imagem),
-                //OpPontuais.bandaBlue(imagem),
-                //OpPontuais.negativo(imagem),
-                //OpPontuais.cinzaRed(imagem),
-                //OpPontuais.cinzaBlue(imagem),
-                //OpPontuais.cinzaGreen(imagem),
-                //OpPontuais.cinzaMedia(imagem),
-                //OpPontuais.binarização(imagem, 80),
-                //OpPontuais.aumentoTonalidade(imagem, "red", 50),
-                //OpPontuais.aumentoTonalidade(imagem, "blue", 50),
-                //OpPontuais.aumentoTonalidade(imagem, "green", 50),
-//                OpPontuais.brilhoAditivoRGB(imagem,90),
-//                OpPontuais.brilhoAditivoBandaY(imagem, 50),
-//                OpPontuais.brilhoMultiplicativoBandaY(imagem, 0.5F),
+
+        ManipulaImagem.exibirImagens(imagem,
                 //OpLocais.media3x3(imagem),
-                OpLocais.mediana3x3(imagem)
-        );
+                OpLocais.mediana3x3(imagem));
 
         ManipulaImagem.salvarImagem(imagem, "jpg", "menino.png");
     }
-
-    //Considerando os seguintes pixels:
-    /*
-
-    Negativo = 255 - bandaPixel
-
-    0,0,0 (preto) -> 255,255,255 (branco)
-    255,255,255 (branco) -> 0,0,0 (preto)
-    100,100,100 -> 155,155,155
-    50,100,150 -> 205,155,105
-
-    Ou seja, para pegar o negativo de um valor X, pega-se 255 e subtrai dele X
-    Se o valor de X é 100, seu inverso é 155
-     */
 
 }
